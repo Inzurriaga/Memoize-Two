d22578608472d75e244c1e199d47a0f0ed74e4ee
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-02-25 20:44:15 -0700

    test for rank and status component complete

--- src/PlayerStatus.test.js
+++ src/PlayerStatus.test.js
@@ -2,8 +2,17 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import PlayerStatus from "./PlayerStatus"
 
+const mockUser = {"name":"gabriel","password":"gabriel","completed":[1,2,5]}
+
 describe("PlayerStatus", ()=> {
-  it("should pass test", () => {
-    expect(true).toEqual(true)
-  })
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(
+            <PlayerStatus user={mockUser}/>
+        )
+    })
+
+    it("shold match the snapshot", () => {
+        expect(wrapper).toMatchSnapshot()
+    })
 })
--- src/RankContainer.test.js
+++ src/RankContainer.test.js
@@ -2,8 +2,18 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import RankContainer from "./RankContainer"
 
+
+const Mockusers = [{"name":"duy","password":"duy","completed":[1,2,5]},{"name":"gabriel","password":"gabriel","completed":[1,2,5]},{"name":"isaac","password":"isaac","completed":[1,2]}]
+
 describe("RankContainer", ()=> {
-  it("should pass test", () => {
-    expect(true).toEqual(true)
-  })
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(
+            <RankContainer users={Mockusers}/>
+        )
+    })
+
+    it("should pass test", () => {
+        expect(wrapper).toMatchSnapshot()
+    })
 })
